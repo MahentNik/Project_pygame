cff5403f85425e2032698395ad803b3113f4d890
Nik <zhmakinkolay@mail.ru>
2022-01-06 01:08:23 +0500

    это ветка в которой все айтемы не объединены родителем

--- items.py
+++ items.py
@@ -1,24 +1,17 @@
 import pygame
 
 
-class Items(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
-    # класс предметов которые жат на земле
-    # их можно подобрать
-
-
-class Coin(Items):
+class Coin(pygame.sprite.Sprite):
     pass
     # класс монета
 
 
-class Gun(Items):
+class Gun(pygame.sprite.Sprite):
     pass
     # класс оружия (если успеем)
 
 
-class Rune(Items):
+class Rune(pygame.sprite.Sprite):
     pass
     # класс руны которая делает тебя невоспреимчивым к урону на 5 сек (кулдаун 25 сек)
     # или дает тебе возможность пройти через тайную текстуру class Secret
